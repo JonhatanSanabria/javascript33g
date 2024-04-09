5dcb54737acbdf94921cba9ab4090921bdf42aa0
Jonhatan Sanabria <jonhatansanabria@gmail.com>
2024-04-08 21:57:23 -0600

    primer commit

---
+++ Practica 4.js
@@ -0,0 +1,57 @@
+
+function myFunction () {
+    return 'Hello There!'
+}
+console.log(myFunction())
+
+const myFunction2 = () => {
+    return 'Hello World!'
+}
+console.log(myFunction2()) // Hello World!
+
+function sumaDosNumeros(num1, num2) {
+    let resultado = num1 + num2
+    return resultado
+} 
+let prueba = sumaDosNumeros(3, 4)
+console.log(prueba)
+
+const addTwoNumbers = (a, b) => a + b
+let prueba2 = addTwoNumbers(5,6)
+console.log(prueba2)
+
+let foo = {
+    name: "John",
+    greeting: function(){
+        console.log(`Hi ${this.name}`);
+    }
+}
+foo.greeting(); // Hi John
+
+//Prototype
+const someString = "hola mundo"
+const someOtherString = "Hola Mundo";
+
+console.log(someString)
+console.log(someString.length)   //accediendo al valor de la propiedad 
+
+console.log(someOtherString)
+console.log(someOtherString.toLowerCase())   // llamar a la funcion
+console.log(someOtherString.toUpperCase())
+console.log(someOtherString.split())
+console.log(someOtherString.split)    // devuelve la definicion de la funcion(metodo)
+
+const phrase = "la mejor manera de aprender es practicando";
+console.log(phrase.length)
+console.log(phrase.split(" ").length)
+console.log(phrase.toUpperCase())
+console.log(phrase.replace("a", "4"))
+
+/* 
+se requiere: 
+    saber cuantos caracteres tiene la frase
+    saber cuantas letras tiene la frase
+    saber cuantas palabras tiene la frase
+    obtener la misma frase pero en mayusculas
+    obtener la misma frase pero con todas las letras "a" reemplazadas por un "4"
+*/
