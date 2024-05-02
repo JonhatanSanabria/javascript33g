79903725e1fc60b178433f4a1abc260eb8a357b7
Jonhatan Sanabria <jonhatansanabria@gmail.com>
2024-05-01 22:24:03 -0600

    delete name

--- Practica 20/js/main.js
+++ Practica 20/js/main.js
@@ -17,31 +17,51 @@ const getKoders = async () => {
         })
         let data = await response.json();
         console.log(data)
-        getKoders()
+        await getKoders()
     }
     
-    
+    const deleteKoderById = async (koderkey) => {
+        let response = await fetch(`https://javascript33g-cd62a-default-rtdb.firebaseio.com/koders/${koderkey}/.json`,{
+            method: 'DELETE'
+        })
+        let data = await response.json()
+        console.log(data);
+    }
+
+
     // Empezamos por activar el botón
     let saveKoderBtn = document.getElementById('save-koder-button')
-    saveKoderBtn.addEventListener('click', () => {
+    saveKoderBtn.addEventListener('click', async () => {
     let inputs = document.querySelectorAll('#koder-form input')
     let koderObject  = {}
     inputs.forEach(({name, value}) => {
         koderObject[name] = value
     })
     console.log(koderObject)
-    postKoder(koderObject)
+    await postKoder(koderObject)
+    await getKoders()
     // Y hacemos que al dar click se borre el formulario
     inputs.forEach((input) => (input.value=''))
 })
 
 // Crear un objeto de koder basado en un array
 const  createKoder = (koderObject) => {
-    let { name, lastname } = koderObject
+    let { name, lastname, key } = koderObject
     let koderItem = document.createElement('li')
-    koderItem.classList.add('list-group-item')
+    koderItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center')
     koderItem.innerText=`${name} ${lastname}`
     
+    let deleteButton = document.createElement('button')
+    deleteButton.classList.add('btn','btn-danger', 'text-center')
+    deleteButton.innerText="Borrar"
+    deleteButton.addEventListener('click', async () => {
+        console.log(key)
+        await deleteKoderById(key)
+        await getKoders()
+    })
+
+    koderItem.append(deleteButton)
+
     return  koderItem;
 }
 
